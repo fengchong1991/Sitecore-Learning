db2b36b3b102356d426bd478b5c0cfff8bd0809e
Chong Feng <chong.feng@adelphi.digital>
2017-07-24 23:49:30 +1000

    no message

---
+++ SitecoreDev.Feature.Media/Repositories/IMediaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreDev.Feature.Media.Repositories
+{
+    public interface IMediaRepository
+    {
+        Item GetItem(string contentGuid);
+    }
+}
---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\Users\fengc\Downloads\frdc_Website\frdc_Website",
+        devRoot: "F:\\Projects\Sitecore",
+        solutionName: "Sitecore",
+        buildConfiguration: "Debug"
+    }
+    return config;
+}
